e607a51dc9b32a8fffd9ca20fd7c810ae6e6c97e
Hxioafei <1253179427@qq.com>
2018-07-11 19:27:17 +0800

    首页公共功能完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -16,4 +16,34 @@ $(function(){
   })
 
 
+  //2点击分类管理2点击分类管理让二级导航显示
+  $(".categroy").on("click",function(){
+    $(this).stop().siblings().slideToggle();
+  })
+
+
+  $(".lt_muen").on("click",function(){
+    $(".lt_aside").toggleClass("active");
+    $("body").toggleClass("active");
+  })
+
+
+  //退出功能
+  $(".loginout").on("click",function(){
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      dataType:"json",
+      success:function(info){
+        //console.log(info)
+        if(info.success){
+          location.href="login.html";
+        }
+      }
+    })
+  })
+
+
+  
+
 })
